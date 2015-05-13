ce67149e492049ab1a9afb0765e734196598a1ae
nevillco <nevillco@bc.edu>
2015-05-12 21:42:47 -0400

    REAL initial commit. Configured AppDelegate to start without storyboard, removed references to Main.storyboard.

--- MyPodium/AppDelegate.m
+++ MyPodium/AppDelegate.m
@@ -7,17 +7,22 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
-
 
 @end
 
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window setRootViewController:[[ViewController alloc] init]];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
